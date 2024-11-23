f5bfb00dd27be6e6c715b44d0227686e208ecb14
evggas <evgeniyagas2000@gmail.com>
2024-11-23 23:57:59 +0300

    finish

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
-from app import db, app
-from app.models import User
+from app import db, app  # Импортируем приложение и базу данных
+from app.models import User  # Импортируем модели
 
-
+# Создаём таблицы
 with app.app_context():
     db.create_all()
-
+    print("База данных и таблицы успешно созданы.")
