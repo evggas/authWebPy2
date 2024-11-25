37d5b8b3c920b0befede8abeb27bae3e0f62cbfd
evggas <evgeniyagas2000@gmail.com>
2024-11-26 01:22:59 +0300

    rassian

--- create_db.py
+++ create_db.py
@@ -2,6 +2,6 @@ from app import db, app  # Импортируем приложение и баз
 from app.models import User  # Импортируем модели
 
 # Создаём таблицы
-with app.app_context():
-    db.create_all()
+with app.app_context():  # Указываем, что работаем в контексте приложения
+    db.create_all()  # Создаём все таблицы из моделей
     print("База данных и таблицы успешно созданы.")
